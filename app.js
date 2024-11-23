552443dae5eaa78ec237cf5d4796bc0cb4dfda9c
Md Habibur Rahman Habib <mhrhabib77232@gmail.com>
2024-11-23 22:50:42 +0600

    select category change event

--- app.js
+++ app.js
@@ -6,68 +6,101 @@ window.onload = () => {
 // Globals
 
 const converter = {
-    dataTransferRate: {
-        name: 'Data Transfer Rate',
-    },
     area: {
         name: 'Area',
+        units: {
+            squareKm: 'Square Kilometer',
+            squareM: 'Square Meter',
+            squareYard: 'Square Yard',
+            squareFoot: 'Square Foot'
+        }
     },
     mass: {
         name: 'Mass',
+        units: {
+            tonne: 'Tonne',
+            kilogram: 'Kilogram',
+            gram: 'Gram',
+            milligram: 'Milligram'
+        }
     },
     length: {
         name: 'length',
+        units: {
+            kilometer: 'Kilometer',
+            meter: 'Meter',
+            centimeter: 'Centimeter',
+            millimeter: 'Millimeter'
+        }
     },
-    volume: {
-        name: 'Volume',
-    },
-    
+    time: {
+        name: 'Time',
+        units: {
+            second: 'Second',
+            minute: 'Minute',
+            hour: 'Hour',
+            day: 'Day'
+        }
+    }
 }
-
 
 
 function main(){
-
-    
-    const converterKeys  = Object.keys(converter).sort()
-
-    
-    // DOM References
     const selectCategory = document.getElementById('select-category')
+    const leftCategory = document.getElementById('left-category')
+    const rightCategory = document.getElementById('right-category')
+
+    const converterKeys  = Object.keys(converter)
+    removeAllChild(selectCategory);
+  
+    converterKeys.forEach((item) => {
+        addOption(selectCategory, {value: item, text: converter[item].name})
+    
+    });
+    
+    
+    
+    selectCategory.addEventListener('change', function(){
+        const selectCategoryValue = selectCategory.value
+        const units = converter[selectCategoryValue].units
+        // handle left site 
+        removeAllChild(leftCategory)
+        const unitsKeys = Object.keys(units)
+        unitsKeys.forEach((item) => {
+            addOption(leftCategory, {value: item, text: units[item]})
+        })
+
+        // handle right site
+        removeAllChild(rightCategory)
+        const unitsKeysLeft = Object.keys(units)
+        unitsKeysLeft.forEach((item) => {
+            addOption(rightCategory, {value: item, text: units[item]})
+        })
+
+        rightCategory.getElementsByTagName('option')[1].selected = 'selected'
+
+        })
     
 
-    // Add event listeners
-    
-
-    // event listener with function call
-
-    // Default call
-    handleConverterKeys(converterKeys,selectCategory)
 }
 
 // event handler
-function handleConverterKeys(converterKeys,selectCategory){
-    deleteAllChild(selectCategory)
-    converterKeys.forEach((item) => {
-        addOption(selectCategory, {value: item,text: converter[item].name})
-
-    })
-}
 
 // DOM function
 
-function addOption(parent, option){
+function addOption(parent, option){  // option = {value: item, text: units[item]}
         
     const opt = document.createElement('option')
     opt.setAttribute('value', option.value)
     opt.innerText = option.text
     parent.appendChild(opt)
+    // console.log(option.value, option.text)
 }
 
-function deleteAllChild(parent){
+function removeAllChild(parent){
     while(parent.firstChild){
         parent.firstChild.remove()
     }
 }
 
-// Units function
+
